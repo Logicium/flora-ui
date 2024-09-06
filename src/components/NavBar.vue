36102213f17fb5c280daae4147b73ca39550d3f2
Kisora Thomas <kisora.thomas@gmail.com>
2024-09-06 11:33:33 -0600

    Gave the Navbar some love and added Icons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
 
+import SearchIcon from "@/components/icons/SearchIcon.vue";
+import ShopIcon from "@/components/icons/ShopIcon.vue";
+import MenuIcon from "@/components/icons/MenuIcon.vue";
+import UserIcon from "@/components/icons/UserIcon.vue";
 </script>
 
 <template>
   <div class="navWrapper">
     <div class="navbar row">
       <div class="title">F L O R A</div>
-      <div>A B O U T</div>
-      <div>B L O G</div>
-      <div>C A R T</div>
-      <div>L O G I N</div>
+      <div class="content">
+        <div>B L O G</div>
+        <div>A B O U T</div>
+        <div>C O N T A C T</div>
+        <div><SearchIcon></SearchIcon></div>
+        <div><ShopIcon></ShopIcon></div>
+        <div><UserIcon></UserIcon></div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,13 +35,21 @@
   width: 50%;
 }
 
+.content{
+  justify-content: space-between;
+  display: flex;
+  width: 50%;
+}
+
 .navbar{
   background-color: whitesmoke;
   display: flex;
   align-items: center;
-  width: 100%;
+  width: 92vw;
   height:6vw;
   font-size: 1.5vw;
+  margin: 0 auto;
 }
+
 
 </style>
---
+++ src/components/icons/MenuIcon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" width="24" height="24" color="#000000">
+    <circle class="cls-6374f8d9b67f094e4896c62c-1" cx="12" cy="3.41" r="1.91"></circle>
+    <circle class="cls-6374f8d9b67f094e4896c62c-1" cx="12" cy="12" r="1.91"></circle>
+    <circle class="cls-6374f8d9b67f094e4896c62c-1" cx="12" cy="20.59" r="1.91"></circle>
+  </svg>
+</template>
+
+<style>
+.cls-6374f8d9b67f094e4896c62c-1{fill:none;stroke:currentColor;stroke-miterlimit:10;}
+</style>
